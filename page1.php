67b12279497c6874ff9a69a16365d2893fea6f27
56650a <97937933+56650a@users.noreply.github.com>
2022-01-25 15:09:14 +0700

    Update page1.php

--- page1.php
+++ page1.php
@@ -2,7 +2,7 @@
         <head>
                 <title>งานที่1</title>
         </head>
-        <body bgcolor="7cffea">
+        <body bgcolor="lightbule">
                 <h1> ประวัติส่วนตัว</h>
                 <br>
                 <br>ชื่อ: </b> <i> ทยิดา เทพคะ</h>
