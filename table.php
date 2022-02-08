0565a83700620f69acc4f9d3d6b0f22b00a263b5
56650a <97937933+56650a@users.noreply.github.com>
2022-02-08 16:02:05 +0700

    Update table.php

--- table.php
+++ table.php
@@ -4,6 +4,7 @@
   </head>
   <body>
     <table border="1">
+    <table bor
       <tr>
         <th>ลำดับ</th>
         <th>ชื่อ - นามสกุล</th>
@@ -15,8 +16,8 @@
          <td>1</td>
          <td>ช็อน จ็อง-กุก </td>
          <td>จองกุก</td>
-         <td><img scr=<img src="https://lh4.googleusercontent.com/nSK313cwJGPJnc-b0nS_2kXLlighqKoyQecyk2fDcK931RfiJyPVsS-qeFK1-5lcBo0y2PF_Kn0x3x3vLxoDYiyUPsQ_fsnoKgdnh6CzkwyFN6r4qhudNW4xjvnjiJVVAQAAFhc" width="130px"></td>
-           <td><a href="https://twitter.com/bts_twt">twitter</a></td>  
+         <td><img scr="https://lh4.googleusercontent.com/nSK313cwJGPJnc-b0nS_2kXLlighqKoyQecyk2fDcK931RfiJyPVsS-qeFK1-5lcBo0y2PF_Kn0x3x3vLxoDYiyUPsQ_fsnoKgdnh6CzkwyFN6r4qhudNW4xjvnjiJVVAQAAFhc" width="130px"></td>
+           <td><a href="https://twitter.com/kookpiics">twitter</a></td>  
       </tr>
      </table>
    </body
