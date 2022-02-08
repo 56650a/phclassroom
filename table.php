1f73dccd8b184e5f177291b653fd781c6d18e3ee
56650a <97937933+56650a@users.noreply.github.com>
2022-02-08 17:29:47 +0700

    Update table.php

--- table.php
+++ table.php
@@ -17,13 +17,41 @@
          <td>จองกุก</td>
          <td><img src="https://pbs.twimg.com/media/EHVTcalUUAA8HE1.jpg" width="130px"></td>
          <td><a href="https://twitter.com/kookpiics">twitter</a></td> 
-        </tr>
+      </tr>
       <tr>
         <td>2</td>
         <td>คิม ซ็อก-จิน </td>
         <td>จิน</td>
         <td><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/36/Kim_Seok-jin_at_the_2018_Korean_Popular_Culture_%26_Arts_Awards_%283%29_%28cropped%29.jpg/330px-Kim_Seok-jin_at_the_2018_Korean_Popular_Culture_%26_Arts_Awards_%283%29_%28cropped%29.jpg" width="130px"></td>
         <td><a href="https://twitter.com/bts_twt">twitter</a></td> 
+      </tr>
+      <tr>
+        <td>3</td>
+        <td>วชิรวิชญ์ ชีวอารี</td> 
+        <td>ไบร์ท</td>
+        <td><img src="https://cdn.readawrite.com/publicassets/3720751/images/350AEC28-712F-4063-927E-D795DC073F08.jpeg" width="130px"></td>
+        <td><a href="https://twitter.com/bbrightvc?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor">twitter</a></td>
+      </tr>
+      <tr>
+        <td>4</td>
+        <td>ยศวรรธน์ ทะวาปี </td> 
+        <td>ยูโร</td>
+        <td><img src="https://us-fbcloud.net/wb/data/1216/1216363-img.u9ad1h.0.jpg" width="130px"></td>
+        <td><a href="https://twitter.com/euroyotsawat">twitter</a></td>
+      </tr>
+      <tr>
+        <td>5</td>
+        <td> ปภาวดี ชาญสมอน</td> 
+        <td>พิ้งค์พลอย</td>
+        <td><img src="https://www.matichon.co.th/wp-content/uploads/2021/04/S__19791874.jpg" width="130px"></td>
+        <td><a href="https://twitter.com/PaparwadeePP">twitter</a></td>
+      </tr> 
+      <tr>
+        <td>6</td>
+        <td>ดาวิกา โฮร์เน่</td> 
+        <td>ใหม่ </td>
+        <td><img src="https://undubzapp.com/wp-content/uploads/2021/10/242742534_578337786645809_4703726039523195712_n.jpg" width="130px"></td>
+        <td><a href="https://twitter.com/DavikaH?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor">twitter</a></td>
       </tr>
      </table>
    </body
