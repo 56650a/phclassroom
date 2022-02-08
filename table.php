53ab0287d310054a4a8b6ac87d4ed78d037c7d58
56650a <97937933+56650a@users.noreply.github.com>
2022-02-08 15:37:40 +0700

    Update table.php

--- table.php
+++ table.php
@@ -15,7 +15,7 @@
          <td>1</td>
          <td>ช็อน จ็อง-กุก </td>
          <td>จองกุก</td>
-         <td><img scr="https://scontent.fbkk5-5.fna.fbcdn.net/v/t1.6435-9/53764592_408899889897681_1264139966204608512_n.jpg?_nc_cat=104&ccb=1-5&_nc_sid=973b4a&_nc_ohc=venfOrewGAIAX9P4B56&_nc_ht=scontent.fbkk5-5.fna&oh=00_AT_srbXgG934UM4ughFIlEM925hp2VQHmfa0ZZSEVU7TGw&oe=6225E6F6 width="130px"></td>
+         <td><img scr="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.tingkorea.com%2Fjungkook-bts-history%2F&psig=AOvVaw2ySyB2ZysgvyUYkjpUJaTp&ust=1644395727457000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCIjni7fZ7_UCFQAAAAAdAAAAABAD" width="130px"></td>
            <td><a href="https://twitter.com/bts_twt">twitter</a></td>  
       </tr>
      </table>
