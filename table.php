0271eee4fe451e2fac254aed16e9bdd1b44c7b05
56650a <97937933+56650a@users.noreply.github.com>
2022-02-08 15:05:47 +0700

    Create table.php

---
+++ table.php
@@ -0,0 +1,25 @@
+<html>
+  <head>
+    <title>ตารางของฉัน< /title>
+  </head>
+  <body>
+    <table border="1">
+      <tr>
+        <th>ลำดับ</th>
+        <th>ชื่อ - นามสกุล</th>
+        <th>ชื่อเล่น< /th>
+        <th>รูป</th>
+        <th>ทวิตเตอร์</th>
+      </tr>
+      <tr>
+         <td>1</td>
+         <td>ช็อน จ็อง-กุก </td>
+         <td>จองกุก</td>
+         <td><img scr="https://scontent.fbkk5-5.fna.fbcdn.net/v/t1.6435-9/53764592_408899889897681_1264139966204608512_n.jpg?_nc_cat=104&ccb=1-5&_nc_sid=973b4a&_nc_ohc=venfOrewGAIAX9P4B56&_nc_ht=scontent.fbkk5-5.fna&oh=00_AT_srbXgG934UM4ughFIlEM925hp2VQHmfa0ZZSEVU7TGw&oe=6225E6F6 width="130px"></td>
+           <td><a href="https://twitter.com/bts_twt">twitter</a></td>  
+      </tr>
+     </table>
+   </body
+</html>
+          
+  
