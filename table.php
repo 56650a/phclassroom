91c5e2c80efb8b47f3b04e22f467d737f56f1642
56650a <97937933+56650a@users.noreply.github.com>
2022-02-08 16:09:07 +0700

    Update table.php

--- table.php
+++ table.php
@@ -15,7 +15,7 @@
          <td>1</td>
          <td>ช็อน จ็อง-กุก </td>
          <td>จองกุก</td>
-         <td><img scr="https://lh4.googleusercontent.com/nSK313cwJGPJnc-b0nS_2kXLlighqKoyQecyk2fDcK931RfiJyPVsS-qeFK1-5lcBo0y2PF_Kn0x3x3vLxoDYiyUPsQ_fsnoKgdnh6CzkwyFN6r4qhudNW4xjvnjiJVVAQAAFhc" width="130px"></td>
+         <td><img scr="https://pbs.twimg.com/media/EHVTcalUUAA8HE1.jpg" width="130px"></td>
          <td><a href="https://twitter.com/kookpiics">twitter</a></td>  
       </tr>
      </table>
