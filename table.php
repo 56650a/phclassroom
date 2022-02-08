73e701110ba3efc7a24fe1c341f8f01b2c9af197
56650a <97937933+56650a@users.noreply.github.com>
2022-02-08 16:27:46 +0700

    Update table.php

--- table.php
+++ table.php
@@ -18,6 +18,13 @@
          <td><img src="https://pbs.twimg.com/media/EHVTcalUUAA8HE1.jpg" width="130px"></td>
          <td><a href="https://twitter.com/kookpiics">twitter</a></td> 
         </tr>
+      <tr>
+        <td>2</td>
+        <td>คิม ซ็อก-จิน </td>
+        <td>จิน</td>
+        <td><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/36/Kim_Seok-jin_at_the_2018_Korean_Popular_Culture_%26_Arts_Awards_%283%29_%28cropped%29.jpg/330px-Kim_Seok-jin_at_the_2018_Korean_Popular_Culture_%26_Arts_Awards_%283%29_%28cropped%29.jpg" width="130px"></td>
+        <td><a href="https://twitter.com/bts_twt">twitter</a></td> 
+      </tr>
      </table>
    </body
 </html>
