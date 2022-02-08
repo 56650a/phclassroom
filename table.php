799d25f5ba60ff2565acf87fe39b859cb51e3742
56650a <97937933+56650a@users.noreply.github.com>
2022-02-08 15:43:52 +0700

    Update table.php

--- table.php
+++ table.php
@@ -15,7 +15,7 @@
          <td>1</td>
          <td>ช็อน จ็อง-กุก </td>
          <td>จองกุก</td>
-         <td><img scr="https://www.tingkorea.com/wp-content/uploads/2021/06/jungkook-against.jpg" width="130px"></td>
+         <td><img scr=https://wikiimg.tojsiabtv.com/wikipedia/commons/thumb/9/9e/Jungkook_for_Dispatch_%22Boy_With_Luv%22_MV_behind_the_scene_shooting,_15_March_2019_07_(cropped).jpg"" width="130px"></td>
            <td><a href="https://twitter.com/bts_twt">twitter</a></td>  
       </tr>
      </table>
