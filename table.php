2f7076a5972955dee311fa0c63dc4284d587c86f
56650a <97937933+56650a@users.noreply.github.com>
2022-02-08 15:41:54 +0700

    Update table.php

--- table.php
+++ table.php
@@ -7,7 +7,7 @@
       <tr>
         <th>ลำดับ</th>
         <th>ชื่อ - นามสกุล</th>
-        <th>ชื่อเล่น< /th>
+        <th>ชื่อเล่น</th>
         <th>รูป </th>
         <th>ทวิตเตอร์</th>
       </tr>
@@ -15,7 +15,7 @@
          <td>1</td>
          <td>ช็อน จ็อง-กุก </td>
          <td>จองกุก</td>
-         <td><img scr="https://sp-ao.shortpixel.ai/client/to_webp,q_lossy,ret_img,w_621/https://www.tingkorea.com/wp-content/uploads/2021/06/jungkook-against.jpg" width="130px"></td>
+         <td><img scr="https://www.tingkorea.com/wp-content/uploads/2021/06/jungkook-against.jpg" width="130px"></td>
            <td><a href="https://twitter.com/bts_twt">twitter</a></td>  
       </tr>
      </table>
