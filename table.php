bbbb7bd09edce7f475e90fff113b8185ace163f7
56650a <97937933+56650a@users.noreply.github.com>
2022-02-08 18:11:12 +0700

    Update table.php

--- table.php
+++ table.php
@@ -29,28 +29,28 @@
         <td>3</td>
         <td>วชิรวิชญ์ ชีวอารี</td> 
         <td>ไบร์ท</td>
-        <td><img src="https://img.pptvhd36.com/thumbor/2020/08/07/5a5fa79da6.jpg" width="130px"></td>
+        <td><img src="https://www.newtv.co.th/images/content/ct_20200525101810780.jpg" width="130px"></td>
         <td><a href="https://twitter.com/bbrightvc?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor">twitter</a></td>
       </tr>
       <tr>
         <td>4</td>
         <td>ยศวรรธน์ ทะวาปี </td> 
         <td>ยูโร</td>
-        <td><img src="https://www.daradaily.com/module/ckfinder/userfiles/images/Euroyotsawat_180831_0001.jpg" width="130px"></td>
+        <td><img src="https://f.ptcdn.info/612/074/000/qyfn906gzqtRvxz9mnC-o.jpg" width="130px"></td>
         <td><a href="https://twitter.com/euroyotsawat">twitter</a></td>
       </tr>
       <tr>
         <td>5</td>
         <td> ปภาวดี ชาญสมอน</td> 
         <td>พิ้งค์พลอย</td>
-        <td><img src="https://www.newtv.co.th/images/content/ct_20210409023904514.jpg" width="130px"></td>
+        <td><img src="https://static.thairath.co.th/media/4DQpjUtzLUwmJZZSCIfgcdfpMugxgjzLITBStsbkLw5s.jpg" width="130px"></td>
         <td><a href="https://twitter.com/PaparwadeePP">twitter</a></td>
       </tr> 
       <tr>
         <td>6</td>
         <td>ดาวิกา โฮร์เน่</td> 
         <td>ใหม่ </td>
-        <td><img src="https://www.tnnthailand.com/static/images/5381e595-ad84-4a8d-a165-f010f4a181fb.jpg" width="130px"></td>
+        <td><img src="https://undubzapp.com/wp-content/uploads/2019/01/1-%E0%B9%83%E0%B8%AB%E0%B8%A1%E0%B9%88-%E0%B8%94%E0%B8%B2%E0%B8%A7%E0%B8%B4%E0%B8%81%E0%B8%B2.jpg" width="130px"></td>
         <td><a href="https://twitter.com/DavikaH?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor">twitter</a></td>
       </tr>
      </table>
