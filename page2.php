cea77ad814dcf1d6ae39a16fad931a6095d92499
56650a <97937933+56650a@users.noreply.github.com>
2022-02-15 14:15:45 +0700

    Update page2.php

--- page2.php
+++ page2.php
@@ -5,6 +5,7 @@
 			#box1{
 				width: 800px;
 				height: 350px;
+				color:yellow;
 			}
 			.box{
 				background-color: lightblue;
@@ -16,7 +17,7 @@
 	</head>
 	<body>
 		<div id="box1" class="box">
-			<h1>นางสาวทยิดา เทพคะ เลขที่ 9 ชั้น ม.4/13</h1>
+			<h1>นางสาวทยิดา เทพคะ เลขที่ 9 ชั้น ม.4/13 </h1>
 		</div>
 	</body>
 </html>
