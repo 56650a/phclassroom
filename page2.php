6c54f9abf33573768bbe8cea6ee582cfce71ca1f
56650a <97937933+56650a@users.noreply.github.com>
2022-02-15 14:30:14 +0700

    Update page2.php

--- page2.php
+++ page2.php
@@ -5,15 +5,13 @@
 			#box1{
 				width: 800px;
 				height: 350px;
+				color: yellow;
 			}
 			.box{
 				background-color: lightblue;
 			}
 			body{
 				background-color: lightpink;
-			}
-			h1{
-				color: yellow;
 			}
 		</style>
 	</head>
