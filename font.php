b856b368438c8ff31aa567e84fd85496fd64a1e7
56650a <97937933+56650a@users.noreply.github.com>
2022-02-22 14:26:42 +0700

    Update font.php

--- font.php
+++ font.php
@@ -11,7 +11,7 @@
     }
     @font-face{
       font-family: myFont2;
-      src: url(phclassroom/FCFlexica[Non-commercial]-Bold.woff)
+      src: url(FCFlexica[Non-commercial]-Bold.woff)
     }
     #font1{
 	font-family: myFont1;    
